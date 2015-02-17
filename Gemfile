604fadb96efcab83927186554063de6c21479731
namenu <namenu@gmail.com>
2015-02-17 18:36:15 +0900

    renew with jekyll

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.2.0'
+gem 'jekyll'
+gem 'RedCloth'
+gem 'rack-jekyll'
+gem 'rake'
+gem 'puma'
---
+++ js/maps.js
@@ -0,0 +1,42 @@
+function initialize() {
+
+  // Create an array of styles.
+  var styles = [];
+
+
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+    
+    var LatLng = new google.maps.LatLng(37.50503, 127.0549725);
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var mapOptions = {
+    scrollwheel: true,
+    navigationControl: false,
+    disableDefaultUI: true,
+    zoom: 16,
+    center: LatLng,
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  
+  var map = new google.maps.Map(document.getElementById('map_canvas'),
+    mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+  
+    var mapMarkerImg = new google.maps.MarkerImage("../img/marker.png");
+    var mapMarker = new google.maps.Marker({
+      position: LatLng,
+      map: map,
+      icon: mapMarkerImg
+  });
+  
+}
