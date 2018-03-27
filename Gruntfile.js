a7dfd358fe70b844e7675d5e3bd2ab97a8b147dc
Hyunwoo Nam <hw.nam@ridi.com>
2018-03-27 19:02:43 +0900

    로컬에서 빌드하고나면 style.css 파일이 달라지는 현상 해결 (style.css는 Jekyll이 사용하는 예약된 이름인 것으로 추측)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,71 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        less: {
-            build: {
-                expand: true,
-                flatten: true,
-                cwd: 'assets/less',
-                src: ['*.less',
-                    '!mixins.less',
-                    '!icon.less',
-                    '!variables.less'
-                ],
-                dest: 'assets/css/',
-                ext: '.css',
-                extDot: 'last'
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['assets/js/<%= pkg.name %>.js'],
-                tasks: ['uglify'],
-                options: {
-                    spawn: false
-                }
-            },
-            less: {
-                files: ['assets/less/*.less'],
-                tasks: ['less'],
-                options: {
-                    spawn: false
-                }
-            }
-        },
-        webfont: {
-            icons: {
-                src: 'icon/svg/*.svg',
-                dest: 'icon/fonts',
-                destCss: 'icon/css',
-                options: {
-                    normalize: true,
-                    font: 'ridicorp-icon',
-                    htmlDemo: true,
-                    htmlDemoTemplate: 'icon/templates/ridicorp_tmpl.html',
-                    destHtml: 'icon',
-                    // stylesheet: 'less',
-                    template: 'icon/templates/ridicorp_tmpl.css',
-                    types: 'eot,ttf,woff',
-                    embed: 'ttf,woff',
-                    fontHeight: 1024,
-                    descent: 64,
-                    templateOptions: {
-                        baseClass: 'ridicorp-icon',
-                        classPrefix: 'icon-',
-                        mixinPrefix: 'icon_'
-                    }
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      build: {
+        expand: true,
+        flatten: true,
+        cwd: 'assets/less',
+        src: ['*.less',
+          '!mixins.less',
+          '!icon.less',
+          '!variables.less'
+        ],
+        dest: 'assets/css/',
+        ext: '.css',
+        extDot: 'last'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['assets/js/<%= pkg.name %>.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false
         }
-    });
+      },
+      less: {
+        files: ['assets/less/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    webfont: {
+      icons: {
+        src: 'icon/svg/*.svg',
+        dest: 'icon/fonts',
+        destCss: 'icon/css',
+        options: {
+          normalize: true,
+          font: 'ridicorp-icon',
+          htmlDemo: true,
+          htmlDemoTemplate: 'icon/templates/ridicorp_tmpl.html',
+          destHtml: 'icon',
+          // stylesheet: 'less',
+          template: 'icon/templates/ridicorp_tmpl.css',
+          types: 'eot,ttf,woff',
+          embed: 'ttf,woff',
+          fontHeight: 1024,
+          descent: 64,
+          templateOptions: {
+            baseClass: 'ridicorp-icon',
+            classPrefix: 'icon-',
+            mixinPrefix: 'icon_'
+          }
+        }
+      }
+    }
+  });
 
-    // Load the plugins.
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-webfont');
+  // Load the plugins.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webfont');
 
 
-    // Default task(s).
-    grunt.registerTask('default', ['less', 'webfont']);
+  // Default task(s).
+  grunt.registerTask('default', ['less', 'webfont']);
 
 };
