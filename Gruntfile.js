1047d671963f59fe2d1f6217440254244c63ce26
krazijames <krazijames@ridi.com>
2018-03-30 14:16:15 +0900

    Split base.less into separate files

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
         }
       },
       less: {
-        files: ['assets/less/*.less'],
+        files: ['assets/less/**/*.less'],
         tasks: ['less'],
         options: {
           spawn: false
